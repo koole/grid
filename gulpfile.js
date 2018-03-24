9ed0305eb27c68106631df6dc76c3821bc42372e
Leon Koole <leon@koole.io>
2018-03-24 13:17:10 +0100

    📦 Only autoprefix in minified version

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
   sass = require('gulp-sass'),
   csso = require('gulp-csso'),
   rename = require('gulp-rename'),
   autoprefixer = require('gulp-autoprefixer');
 
 
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   gulp.src('sass/grid.sass')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/'))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest('dist/'))
     .pipe(csso())
     .pipe(rename({
       suffix: '.min'
