3bbb1d03755e1b3748762842eaad468ca4396aff
Leon Koole <leon@koole.io>
2019-05-08 12:31:57 +0200

    Update gulpfile for Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
-const gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  csso = require('gulp-csso'),
-  rename = require('gulp-rename'),
-  autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  csso = require("gulp-csso"),
+  rename = require("gulp-rename"),
+  autoprefixer = require("gulp-autoprefixer");
 
-
-gulp.task('sass', () => {
-  gulp.src('sass/grid.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/'))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
+function css() {
+  return gulp
+    .src("sass/grid.sass")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist/"))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
     .pipe(csso())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('dist/'));
-});
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(gulp.dest("dist/"));
+}
 
-gulp.task('default', ['sass']);
+gulp.task("default", gulp.parallel(css));
